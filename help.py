48e09340d24e37989c032e7b16520aa006626086
Lmzi <89996787+Lofnheid@users.noreply.github.com>
2021-11-06 18:08:51 +0530

    Update help.py

--- help.py
+++ help.py
@@ -29,16 +29,27 @@ Gives information about the guild
           await ctx.send(embed=embed)
       elif argument.lower()=='moderator':
           embed=discord.Embed(title="Moderator Commands", description='''`-ban [member] (optional:reason)`
-Bans a member from the server
+Bans a member from the server.
 
 `-unban [member]`
-Unbans a pre-banned member from the server
+Unbans a pre-banned member from the server.
 
 `-kick [member] (optional reason)`
-Kicks a member from the server
+Kicks a member from the server.
 
 `-mute [member] (optional:reason)`
-Temporarily mutes a member in the server
+Temporarily mutes a member in the server.
+''', color=0x109319)
+          await ctx.send(embed=embed)
+      elif argument.lower()=='waifu' or argument.lower()=='anime':
+          embed=discord.Embed(title="Anime/Waifu Commands", description='''`-waifu`
+Display an image with the "waifu" tag.
+
+`-maid`
+Display an image with the "maid" tag.
+
+`-random`
+Display an image with the "all" tag.
 ''', color=0x109319)
           await ctx.send(embed=embed)
       elif argument.lower()=='music':
@@ -106,13 +117,15 @@ Type "-help utility" will give more info about all the bot commands.''', inline=
 Type "-help moderator" to get more info about all the moderator commands.''', inline=False)
           embed.add_field(name="3. :musical_note:Music(Delayed):", value='''`join`, `play`, `pause`, `resume`, `repeat`, `download`, `queue`, `reset`, `skip`, `song-info`, `volume`, `stop`, `leave`
 
-Type "-help music" to get more info about all the music commands.''', inline=False)
+Type "-help waifu" to get more info about all the music commands.''', inline=False)
+          embed.add_field(name="4. Anime/Waifu:", value='''`waifu`, `maid`, `random`
 
-          embed.add_field(name="4. :robot:About Bot and Dev(Just for fun)", value='''`aboutdev`, `ping`, `invite`
+Type "-help music" to get more info about all the music commands.''', inline=False)
+          embed.add_field(name="5. :robot:About Bot and Dev(Just for fun)", value='''`aboutdev`, `ping`, `invite`
 
 Type "-help aboutdev" to get more info about all the About commands.
 [**Invite Link**](https://discord.com/api/oauth2/authorize?client_id=865876260748132353&permissions=8&scope=bot)  ||   [**Support Server**](https://discord.gg/ReTdc9dFuP)''', inline=False)
-          embed.set_footer(text=f'''Harmoni Bot version-Beta 1.3. More features will be added in the upcoming version.
+          embed.set_footer(text=f'''Harmoni Bot version-Beta 1.4. More features will be added in the upcoming version.
 Requested by:{member}''', icon_url=member.avatar_url)
           await ctx.send(embed=embed)
 
